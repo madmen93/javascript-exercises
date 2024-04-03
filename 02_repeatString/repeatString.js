03788f77801178092dcc01e2de5488869605a2ff
Monica Daza <158379665+madmen93@users.noreply.github.com>
2024-04-03 16:29:06 -0500

    ejercicio completado

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,18 @@
-const repeatString = function() {
+const repeatString = function(text, n) {
+
+    let errorText = 'ERROR';
+
+    if(n >= 0){
+        let finalText = text.repeat(n);
+        return finalText;
+    }else if(n < 0){
+        return errorText;
+    }
+
+
+
+
+
 
 };
 
