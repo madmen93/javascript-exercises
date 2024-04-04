817d714fc05e2230c266fa224d198448a80033fc
Monica Daza <158379665+madmen93@users.noreply.github.com>
2024-04-04 15:10:28 -0500

    ejercicio completado

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    const arr = [];
+    let num = text.length -1;
+    for(i = 0; i < text.length; i++){
+        arr[i] = text.charAt(num--);
+    }
+    newText = arr.join("");
+    return newText;
 };
 
 // Do not edit below this line
