e1f07791da4835c35f06fc0d916a9514e6ec3596
Monica Daza <158379665+madmen93@users.noreply.github.com>
2024-04-04 17:50:17 -0500

    ejercicio completado

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
 const removeFromArray = function() {
+    firstArg = arguments[0];
+    for(i = 1; i < arguments.length; i++){
+        if(firstArg.includes(arguments[i])){
+            do{
+                let num = firstArg.indexOf(arguments[i]);
+                let remove = firstArg.splice(num, 1);
+            }while(firstArg.includes(arguments[i]));
+        }
+    }
+    return firstArg;
+    };
 
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
