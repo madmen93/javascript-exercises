b39d4114470ab991a700bdcce95a07c6a0123a25
Monica Daza <158379665+madmen93@users.noreply.github.com>
2024-04-08 14:59:16 -0500

    ejercicio resuelto

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+c = (f - 32) * 5 / 9;
+cRounded = Math.round(c * 10) / 10;
+return cRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+f = (c * (9 / 5) + 32);  
+fRounded = Math.round(f * 10) /10;
+return fRounded;
 };
+
 
 // Do not edit below this line
 module.exports = {
