da521f29b489ea63b44602ccb6e66d270583cac1
Monica Daza <158379665+madmen93@users.noreply.github.com>
2024-05-09 10:45:43 -0500

    exercise 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, sum) => total + sum, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+return arr.reduce((total, mult) => total * mult, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if(n == 0)
+    return 1;
+  else{
+    let total = 1;
+    for(i = n; i > 0; i--){
+      total *= i;
+  }
+  return total;
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let word = string.toLowerCase().split('');
+    for(let i = 0; i < word.length; i++){
+        if(word[i] == ' '){
+            word.splice(i, 1);
+        }}
+    for(let i = 0; i < word.length; i++){
+        if((word[i].charCodeAt() < 48 || word[i].charCodeAt() > 57) && (word[i].charCodeAt() < 97 || word[i].charCodeAt() > 122)){
+            word.splice(i, 1);
+        }
+    }
+    let newString = word.join('');
+    let reverseWord = word.reverse().join('');
+if(newString === reverseWord)
+    return true;
+else
+    return false;
 };
 
 // Do not edit below this line
