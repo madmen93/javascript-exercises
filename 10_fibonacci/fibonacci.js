98c4d3c99fca6b02f77389e1149f8d315ac4256d
Monica Daza <158379665+madmen93@users.noreply.github.com>
2024-05-09 12:07:23 -0500

    exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+/* const fibonacci = function(num) {
+    let array = [0, 1];
+    for(let i = 2; i <= num; i++){
+        array[i] = array[(i-2)] + array[(i-1)];
+    }
+    return array[num];
+};
+ */
+const fibonacci = function(num) {
+    let array = [0, 1];
+    if(num >= 0){
+        for(let i = 2; i <= num; i++){
+            array.push((array[(i-2)]+array[(i-1)]))
+        }
+        return array[num];
+    }else{
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
