604ca5304ad699e3ac44994aa983cec7ea4db666
Monica Daza <158379665+madmen93@users.noreply.github.com>
2024-05-13 15:41:56 -0500

    Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, currentperson) => {
+        const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        const currentAge = getAge(currentperson.yearOfDeath, currentperson.yearOfBirth);
+        return oldestAge < currentAge ? currentperson : oldest;
+    })
 };
 
+const getAge = function(death, birth){
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
